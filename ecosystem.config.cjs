54bfe23cb2ff96f8e2ce7ece8d048731abe01c2a
harsh-mind23 <hkhare708@gmail.com>
2026-02-21 14:52:54 +0530

    Switch to cluster mode

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,15 +3,15 @@ module.exports = {
     {
       name: 'mindmanthan',
       script: 'main.js',
-      instances: 1,
-      exec_mode: 'fork',
+      instances: 'max',   
+      exec_mode: 'cluster',
       env: {
         NODE_ENV: 'development',
         PORT: 5173
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 5173
+        PORT: 3000
       }
     }
   ]
